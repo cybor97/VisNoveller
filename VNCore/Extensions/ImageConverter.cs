a2893577422da5898bfb3c16a44e5429c713893c
Ruslan <muhametshin.1997@gmail.com>
2016-05-16 22:19:18 +0300

    Added ImageConverter extension

---
+++ VNCore/Extensions/ImageConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace VNCore.Extensions
+{
+    public static class BitmapConverter
+    {
+        public static byte[] ToByteArray(this Icon icon)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                icon.ToBitmap().Save(ms, ImageFormat.Png);
+                return ms.GetBuffer();
+            }
+        }
+        public static Icon ToIcon(this byte[] bytes)
+        {
+            using (MemoryStream ms = new MemoryStream(bytes)) return Icon.FromHandle(new Bitmap(ms).GetHicon());
+        }
+        public static byte[] ToByteArray(this Bitmap image)
+        {
+            MemoryStream ms = new MemoryStream();
+            image.Save(ms, ImageFormat.Png);
+            return ms.GetBuffer();
+        }
+        public static Bitmap ToBitmap(this byte[] ByteArray)
+        {
+            return (Bitmap)Image.FromStream(new MemoryStream(ByteArray)); ;
+        }
+    }
+}
