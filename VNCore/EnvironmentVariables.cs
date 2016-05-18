1159eec192e069b9c80706820778c8757bb856b3
Ruslan <muhametshin.1997@gmail.com>
2016-05-18 15:40:15 +0300

    Implemented environment variables

---
+++ VNCore/EnvironmentVariables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace VNCore
+{
+    public static class EnvironmentVariables
+    {
+        static string DataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "VisNoveller");
+        public static string ConfigurationFile = Path.Combine(DataDirectory, "Config.xml");
+    }
+}
