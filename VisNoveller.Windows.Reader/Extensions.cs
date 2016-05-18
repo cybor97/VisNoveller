2911150442738690bb4d5b68a8f2a1a3c7528b8f
Ruslan <muhametshin.1997@gmail.com>
2016-05-18 22:24:42 +0300

    Made window fullscreen by default Implemented logo animations

---
+++ VisNoveller.Windows.Reader/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VisNoveller.Windows.Reader
+{
+	public static class Extensions
+	{
+		static Random Magic = new Random();
+
+		public static string GetRandomString(int length, bool cyrillic = false)
+		{
+			string result = string.Empty;
+			for (int i = 0; i < length; i++)
+				if (cyrillic)
+					result += (char)Magic.Next('А'-5, 'я'+5);
+				else
+					result += (char)Magic.Next('A'-5, 'z'+5);
+			return result;
+		}
+	}
+}
+
---
+++ VisNoveller.Windows.Reader/LogoScreen.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace VisNoveller.Windows.Reader
+{
+    public partial class LogoScreen : Grid
+    {
+        Random Magic { get; set; }
+        int SlideNumber = 0;
+        public LogoScreen()
+        {
+            InitializeComponent();
+            Magic = new Random();
+            Loaded += (s, EA) => CoolTextEffect("KAD_TECHi");
+
+        }
+        void CoolTextEffect(string baseString)
+        {
+            Opacity = 1;
+            LogoTB.Foreground = Brushes.Lime;
+            Background = Brushes.Black;
+            var displayString = "";
+            new Thread(() =>
+            {
+                displayString = Extensions.GetRandomString(baseString.Length);
+                while (OffsetString(baseString, ref displayString))
+                    Dispatcher.Invoke((Action)(() => LogoTB.Text = displayString), DispatcherPriority.Background);
+                Dispatcher.Invoke((Action)(() => LogoTB.Text = displayString), DispatcherPriority.Background);
+                Thread.Sleep(2000);
+                Dispatcher.Invoke((Action)(() => ((Storyboard)Resources["FlashStoryboard"]).Begin()), DispatcherPriority.Background);
+            }).Start();
+            SlideNumber++;
+        }
+        bool OffsetString(string baseString, ref string displayString)
+        {
+            for (int i = 0; i < displayString.Length; i++)
+            {
+                var arr = displayString.ToCharArray();
+                if (displayString[i] < baseString[i])
+                    arr[i]++;
+                else if (displayString[i] > baseString[i])
+                    arr[i]--;
+                displayString = new string(arr);
+                Thread.Sleep(5);
+            }
+            return displayString != baseString;
+        }
+
+        private void DisappearAnimation_Completed(object sender, EventArgs e)
+        {
+            if (SlideNumber == 1)
+            {
+                LogoTB.FontFamily = new FontFamily("Arial");
+                LogoTB.FontSize *= 2;
+                ((Storyboard)Resources["DisappearStoryboard"]).AutoReverse = false;
+                CoolTextEffect("VisNoveller");
+            }
+        }
+    }
+}
