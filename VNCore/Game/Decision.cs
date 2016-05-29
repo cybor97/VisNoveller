077b5b9e5bca7a96005e51b855f29570cf79e1fd
Ruslan <muhametshin.1997@gmail.com>
2016-05-29 15:22:46 +0300

    Implemented decisions saving

---
+++ VNCore/Game/Decision.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace VNCore.Game
+{
+    public class Decision
+    {
+        public int SlideID { get; set; }
+        public int DecisionID { get; set; }
+        public override string ToString()
+        {
+            var stream = new MemoryStream();
+            using (var writer = XmlWriter.Create(stream))
+            {
+                writer.WriteStartElement("Decision");
+                writer.WriteAttributeString("SlideID", SlideID.ToString());
+                writer.WriteAttributeString("DecisionID", DecisionID.ToString());
+                writer.WriteEndElement();
+            }
+            return Encoding.UTF8.GetString(stream.GetBuffer());
+        }
+        public static Decision Parse(string xml)
+        {
+            var result = new Decision();
+            using (var reader = XmlReader.Create(new MemoryStream(Encoding.UTF8.GetBytes(xml))))
+                while (reader.Read())
+                    if (reader.IsStartElement("Decision"))
+                    {
+                        result.SlideID = int.Parse(reader.GetAttribute("SlideID"));
+                        result.DecisionID = int.Parse(reader.GetAttribute("DecisionID"));
+                    }
+            return result;
+        }
+    }
+}
--- VNCore/Game/SavedGame.cs
+++ VNCore/Game/SavedGame.cs
@@ -1,14 +1,20 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Xml;
 
-namespace VNCore.Saves
+namespace VNCore.Game
 {
-    class SavedGame
+    public class GameState
     {
         public string Name { get; set; }
         public string NovelPath { get; set; }
         public int SlideID { get; set; }
+        public List<Decision> Decisions { get; set; }
+        public GameState()
+        {
+            Decisions = new List<Decision>();
+        }
         public override string ToString()
         {
             var stream = new MemoryStream();
@@ -18,12 +24,15 @@ namespace VNCore.Saves
                 writer.WriteAttributeString("Name", Name);
                 writer.WriteAttributeString("NovelPath", NovelPath);
                 writer.WriteAttributeString("SlideID", SlideID.ToString());
+                foreach (var current in Decisions)
+                    writer.WriteRaw(current.ToString());
+                writer.WriteEndElement();
             }
             return Encoding.UTF8.GetString(stream.GetBuffer());
         }
-        public static SavedGame Parse(string xml)
+        public static GameState Parse(string xml)
         {
-            var result = new SavedGame();
+            var result = new GameState();
             using (var reader = XmlReader.Create(new MemoryStream(Encoding.UTF8.GetBytes(xml))))
                 while (!reader.EOF)
                     if (reader.IsStartElement("Game"))
@@ -32,7 +41,11 @@ namespace VNCore.Saves
                         result.NovelPath = reader.GetAttribute("NovelPath");
                         int slideID;
                         slideID = int.TryParse(reader.GetAttribute("SlideID"), out slideID) ? slideID : 0;
+                        reader.Read();
                     }
+                    else if (reader.IsStartElement("Decision"))
+                        result.Decisions.Add(Decision.Parse(reader.ReadOuterXml()));
+                    else reader.Read();
             return result;
         }
     }
